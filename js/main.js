467423cf67d3467bbbb9a27e57ea2a51d12289ba
CharlyX <jclopex@gmail.com>
2020-12-07 22:28:34 -0600

    ADD prompt, console.log, parse.int & float, operadores

--- js/main.js
+++ js/main.js
@@ -1,5 +1,47 @@
 console.log('hola koders')
 
+//variables
+// let, var, const
+// let: si va cambiar de valor
+// const: si es un valor fijo y se declara en MAYUSCULAS
 
+/* var nombreVariable;
+let nombreVariable2;
+
+let nombreVariable2 = 'Algo';
+let numero = 1000;
+let boleano = true;
+let myArray = [1, '2', true, 4];
+let myObjectKoder = {
+    nombre: 'Carlos',
+    mail: 'jclopex@gmail.com',
+    github: '../jclopezp'
+} */
+
+/*
+rem - root Element en relación al elemento root
+em - 
+pageXOffsetch
+visual
+Viewport
+%
+pc - picas
+pt - puntos
+*/
+
+let name_koder = prompt ('¿Cuál es tu nombre?');
+console.log (name_koder)
+let age_koder = prompt ('¿cuál es tu edad?')
+console.log (age_koder)
+
+let age_int = parseInt (age_koder);
+let age_int2 = parseFloat (age_koder);
+
+console.log('suma: ', (age_int) + 5)
+
+
+console.log('multi: ', (age_int) * 5);
+console.log('div: ', parseInt(age_int) / 5);
+console.log('resta: ', parseInt(age_int2) - 5);
 
 
