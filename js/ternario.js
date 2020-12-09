5b7729420bc097ea398955fc508c529ad102df28
CharlyX <jclopex@gmail.com>
2020-12-08 22:02:06 -0600

    ADD ternario

---
+++ js/ternario.js
@@ -0,0 +1,19 @@
+console.log('ready')
+
+// if ternario
+
+/* expression
+let myExpresion = true
+
+expression ? si true : si false */
+
+// Comparar si el primer numero > que el segundo
+
+const NUMBER_ONE = parseInt(prompt('Ingresa un número'))
+const NUMBER_TWO = parseInt(prompt('Ingresa otro número'))
+
+NUMBER_ONE > NUMBER_TWO ?
+console.log(`El primer número [ ${NUMBER_ONE} ] es mayor`)
+:
+console.log(`El segundo número [ ${NUMBER_TWO} ] es mayor`)
+
