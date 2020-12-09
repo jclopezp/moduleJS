591adffbe5ebdc3ca81da16b9677676d30d91ff2
CharlyX <jclopex@gmail.com>
2020-12-08 19:29:48 -0600

    add calculo de peso en la luna

---
+++ js/weight_in_moon.js
@@ -0,0 +1,8 @@
+let weight = prompt ('¿Cuál es tu peso?');
+console.log (weight);
+
+let weight_int = parseInt (weight);
+
+let weight_moon = (weight_int * (1/6));
+
+alert ("tu peso en la luna es: " + weight_moon);
