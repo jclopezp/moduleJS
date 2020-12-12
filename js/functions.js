cb2f23c2730c42e518713f8ca322d4ce8a86afc7
CharlyX <jclopex@gmail.com>
2020-12-11 21:12:10 -0600

    class 04

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+
+function nombreFuncion (parametros, ...) {
+    //sentencias
+}
+
+function nombreFuncion () {
+    let nombre = prompt('Cual es tu nombre?')
+    console.log(nombre)
+}
+
+function backwardName (name, lastName) {
+    console.log(name, lastName)
+}
+
+function add (valor1, valor2) {
+    console.log(valor1 + valor2)
+}
+
+// Parametros cuando se define
+// Argumentos cuando se manda a llamar
--- js/main.js
+++ js/main.js
@@ -1,5 +1,69 @@
 console.log('hola koders')
 
+for (let i = 10; i >= 1; i--){
+    console.log(`8 x ${i} = ${8 * i}`)
+}
+
+//Imprimir numeros pares en el rango de 1 a 100
+
+for (let i = 1, i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+let counter = 1
+while (counter <= 100) {
+    if(counter % 2 === 0) {
+        console.log(counter)
+}
+counter++
+}
+
+//Reverse invertir el orden
+const stringToReverse = "Hola mundo"
+let stringReversed
+for (let i = stringToReverse.length - 1; i >= 0 ; i-- ) {
+    stringReversed += stringReversed[i]
+}
+console.log(stringReversed)
+
+// 
+
+while (condicion) {
+    //sentencia
+
+    // muta la condicion o variable
+}
+
+//---------------------- 
+// imprimir la tabla del 7 con while
+
+let counter= 1
+while (counter <= 10) {
+    //sentencia
+    console.log(`7 x ${counter} = ${7 * counter}`)
+    // muta la condicion o variable
+    counter = counter + 1
+}
+
+
+
+do {
+    //sentencia
+
+    // muta la condicion o variable
+} while(condicion)
+
+//---------------------- 
+// imprimir la tabla del 7 con do
+
+let counterDo = 1
+do {
+    //sentencias
+    console.log(`7 x ${counterDo} = ${7 * counterDo}`)
+    counterDo++
+} while(counterDo < 10)
 
 
 
